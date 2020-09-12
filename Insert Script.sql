483e9df68ccab4277fefe69a6c39968eed5be2b9
hemantCC <49364193+hemantCC@users.noreply.github.com>
2020-09-12 09:29:47 +0530

    Add files via upload

---
+++ Insert Script.sql
@@ -0,0 +1,64 @@
+INSERT INTO tblContractType VALUES ('Full Time'),('Not Applicable'),('Part Time'),('Self-Employed')
+INSERT INTO tblDocumentType VALUES ('Salary Slip'), ('Driving License'), ('Certificate Of Residence'), ('Identification Proof')
+INSERT INTO tblEmploymentStatus VALUES ('Employee'), ('Functionary'), ('Retired'), ('Non-Qualified Employee')
+INSERT INTO tblIncludedServices VALUES ('Maintenance and Repairs'),('Summer Tyres (normal wear and tear)'),('Winter Tyres (normal wear and tear)'),('Assistance'),('Road tax'),('Insurance'),('Immediate Relief Car')
+INSERT INTO tblInsurance VALUES (12,1000),(24,2000),(36,3000),(48,4000),(60,5000),(72,6000)
+INSERT INTO tblMaritalStatus VALUES ('Legally Cohabitating'),('Married'),('Single')
+INSERT INTO tblMileage VALUES (10000),(15000),(20000),(25000),(30000)
+INSERT INTO tblNationality VALUES ('Austria'),('Belgium'),('China'),('Denmark'),('France'),('Germany'),('India'),('Ireland'),('Italy'),('Japan'),('Luxembourg'),('Neitherlands'),('Norway'),('Poland'),('Portugal'),('South Africa'),('Spain'),('Sweden'),('Switzerland'),('United Kingdom'),('United States')
+INSERT INTO tblPropertyStatus VALUES ('Co-Tenant'),('Home Owner'),('Tenant')
+INSERT INTO tblQuoteStatus VALUES ('Pending'),('Accepted'),('Rejected')
+
+INSERT INTO tblVehicleDetail VALUES (N'bmw-x1.jpg', N'HATCH', N'BMW', N'BMW-1', N'221', N'5.4', N'122', N'Diesel', 4, 1, 2, N'Manual', N'White', 425)
+INSERT INTO tblVehicleDetail VALUES (N'porche.jpg', N'COUPE', N'PORCHE', N'PORCHE CAYENNE', N'138', N'4', N'99', N'Diesel', 4, 1, 2, N'Manual', N'grey', 42)
+INSERT INTO tblVehicleDetail VALUES (N'fordfiesta.jpg', N'SEDAN', N'FORD', N'FIESTA', N'200', N'4.4', N'100', N'Petrol', 4, 1, 1, N'Manual', N'white', 359)
+INSERT INTO tblVehicleDetail VALUES (N'renaultclio.jpg', N'SEDAN', N'RENAULT', N'CLIO V', N'150', N'4.3', N'99', N'Diesel', 4, 1, 3, N'Manual', N'red', 342)
+INSERT INTO tblVehicleDetail VALUES (N'minicooper.jpg', N'COUPE', N'MINI COOPER', N'MINI 2018', N'150', N'5', N'114', N'Petrol', 2, 1, 1, N'Auto', N'grey', 378)
+INSERT INTO tblVehicleDetail VALUES (N'bmw-x2.jpg', N'HATCHBACK', N'BMW', N'BMW X2', N'230', N'5.5', N'125', N'Diesel', 4, 1, 4, N'Manual', N'green', 457)
+INSERT INTO tblVehicleDetail VALUES (N'mercedes-class-cla.jpg', N'COUPE', N'MERCEDES', N'BENZ CLASS CLA', N'250', N'5.3', N'122', N'Petrol', 4, 1, 4, N'Auto', N'grey', 557)
+INSERT INTO tblVehicleDetail VALUES (N'mercedes-class-glc.jpg', N'COUPE', N'MERCEDES', N'BENZ CLASS GLC', N'250', N'6.6', N'151', N'Diesel', 4, 1, 1, N'Auto', N'red', 822)
+INSERT INTO tblVehicleDetail VALUES (N'bmw-3.jpg', N'SEDAN', N'BMW', N'BMW 2019', N'200', N'1.6', N'34', N'Petrol', 4, 1, 2, N'Auto', N'white', 594)
+INSERT INTO tblVehicleDetail VALUES (N'bmw-x3.jpg', N'SUV', N'BMW', N'BMW 2017', N'250', N'6.7', N'154', N'Petrol', 4, 1, 4, N'Auto', N'black', 742)
+INSERT INTO tblVehicleDetail VALUES (N'jaguar.jpg', N'COUPE', N'JAGUAR', N'JAGUAR E-PACE', N'200', N'8', N'182', N'Diesel', 4, 1, 2, N'Auto', N'white', 641)
+INSERT INTO tblVehicleDetail VALUES (N'nissan.jpg', N'COUPE', N'NISSAN', N'JUKE 2020', N'150', N'5.9', N'135', N'Petrol', 4, 1, 2, N'Auto', N'red', 427)
+INSERT INTO tblVehicleDetail VALUES (N'volvo-xc40.jpg', N'SUV', N'VOLVO', N'VOLVO-XC40', N'250', N'6.6', N'149', N'Diesel', 4, 1, 2, N'Auto', N'grey', 580)
+INSERT INTO tblVehicleDetail VALUES (N'volvo-xc60.jpg', N'SUV', N'VOLVO', N'VOLVO-XC60', N'250', N'7.1', N'165', N'Petrol', 4, 1, 4, N'Auto', N'black', 717)
+INSERT INTO tblVehicleDetail VALUES (N'landrover-velar.jpg', N'SUV', N'LANDROVER', N'VELAR', N'208', N'7.6', N'173', N'Petrol', 4, 1, 2, N'Auto', N'grey', 918)
+INSERT INTO tblVehicleDetail VALUES (N'landrover-evoque.jpg', N'SUV', N'LANDROVER', N'EVOQUE', N'250', N'7.6', N'173', N'Diesel', 4, 1, 2, N'Auto', N'black', 800)
+
+INSERT INTO tblEquipment VALUES 
+(1,'Main','Automatic Air Conditioning,Three modes of Drive,Speed regulator,Navigation Supplement'),
+(1,'Standard','Steering wheel adjustable in depth,ECO-mode function,Heated exterior mirrors,Tire pressure monitoring system,Automatic door locking while driving,Rear side airbags,Electric AV windows,Cruise control - speed limiter,Body-color door handles,Folding key with 3 button'),
+(2,'Main','Imitation leather steering wheel,LED rear lights,Heated exterior mirrors,Tire pressure monitoring system,Rear side airbags,Lane Keeping Assistant,Cruise control - speed limiter,Body-color door handles,Electric AV windows,Driver-side vanity mirror'),
+(2,'Standard','Manual Air Conditioner,Business Pack,Model Advantage'),
+(3,'Main','Level 1 Air Conditioning,Model Advantage,Speed regulator,Three modes of Drive'),
+(3,'Standard','Electric AV windows,Imitation leather steering wheel,Heated exterior mirrors,Tire pressure monitoring system, Front seatbelt with pretensioners + force limiters, Lane Keeping Assistant,Cruise control - speed limiter, Body - color door handles, LED rear lights, Driver - side vanity mirror'),
+(4,'Main','Automatic Air Conditioning,Three modes of Drive,Speed regulator,Navigation Supplement'),
+(4,'Standard','Steering wheel adjustable in depth,ECO-mode function,Heated exterior mirrors,Tire pressure monitoring system,Automatic door locking while driving,Rear side airbags,Electric AV windows,Cruise control - speed limiter,Body-color door handles,Folding key with 3 button'),
+(5,'Main','Imitation leather steering wheel,LED rear lights,Heated exterior mirrors,Tire pressure monitoring system,Rear side airbags,Lane Keeping Assistant,Cruise control - speed limiter,Body-color door handles,Electric AV windows,Driver-side vanity mirror'),
+(5,'Standard','Manual Air Conditioner,Business Pack,Model Advantage'),
+(6,'Main','Level 1 Air Conditioning,Model Advantage,Speed regulator,Three modes of Drive'),
+(6,'Standard','Electric AV windows,Imitation leather steering wheel,Heated exterior mirrors,Tire pressure monitoring system, Front seatbelt with pretensioners + force limiters, Lane Keeping Assistant,Cruise control - speed limiter, Body - color door handles, LED rear lights, Driver - side vanity mirror'),
+(7,'Main','Automatic Air Conditioning,Three modes of Drive,Speed regulator,Navigation Supplement'),
+(7,'Standard','Steering wheel adjustable in depth,ECO-mode function,Heated exterior mirrors,Tire pressure monitoring system,Automatic door locking while driving,Rear side airbags,Electric AV windows,Cruise control - speed limiter,Body-color door handles,Folding key with 3 button'),
+(8,'Main','Imitation leather steering wheel,LED rear lights,Heated exterior mirrors,Tire pressure monitoring system,Rear side airbags,Lane Keeping Assistant,Cruise control - speed limiter,Body-color door handles,Electric AV windows,Driver-side vanity mirror'),
+(8,'Standard','Manual Air Conditioner,Business Pack,Model Advantage'),
+(9,'Main','Level 1 Air Conditioning,Model Advantage,Speed regulator,Three modes of Drive'),
+(9,'Standard','Electric AV windows,Imitation leather steering wheel,Heated exterior mirrors,Tire pressure monitoring system, Front seatbelt with pretensioners + force limiters, Lane Keeping Assistant,Cruise control - speed limiter, Body - color door handles, LED rear lights, Driver - side vanity mirror'),
+(10,'Main','Automatic Air Conditioning,Three modes of Drive,Speed regulator,Navigation Supplement'),
+(10,'Standard','Steering wheel adjustable in depth,ECO-mode function,Heated exterior mirrors,Tire pressure monitoring system,Automatic door locking while driving,Rear side airbags,Electric AV windows,Cruise control - speed limiter,Body-color door handles,Folding key with 3 button'),
+(11,'Main','Imitation leather steering wheel,LED rear lights,Heated exterior mirrors,Tire pressure monitoring system,Rear side airbags,Lane Keeping Assistant,Cruise control - speed limiter,Body-color door handles,Electric AV windows,Driver-side vanity mirror'),
+(11,'Standard','Manual Air Conditioner,Business Pack,Model Advantage'),
+(12,'Main','Level 1 Air Conditioning,Model Advantage,Speed regulator,Three modes of Drive'),
+(12,'Standard','Electric AV windows,Imitation leather steering wheel,Heated exterior mirrors,Tire pressure monitoring system, Front seatbelt with pretensioners + force limiters, Lane Keeping Assistant,Cruise control - speed limiter, Body - color door handles, LED rear lights, Driver - side vanity mirror'),
+(13,'Main','Automatic Air Conditioning,Three modes of Drive,Speed regulator,Navigation Supplement'),
+(13,'Standard','Steering wheel adjustable in depth,ECO-mode function,Heated exterior mirrors,Tire pressure monitoring system,Automatic door locking while driving,Rear side airbags,Electric AV windows,Cruise control - speed limiter,Body-color door handles,Folding key with 3 button'),
+(14,'Main','Imitation leather steering wheel,LED rear lights,Heated exterior mirrors,Tire pressure monitoring system,Rear side airbags,Lane Keeping Assistant,Cruise control - speed limiter,Body-color door handles,Electric AV windows,Driver-side vanity mirror'),
+(14,'Standard','Manual Air Conditioner,Business Pack,Model Advantage'),
+(15,'Main','Level 1 Air Conditioning,Model Advantage,Speed regulator,Three modes of Drive'),
+(15,'Standard','Electric AV windows,Imitation leather steering wheel,Heated exterior mirrors,Tire pressure monitoring system, Front seatbelt with pretensioners + force limiters, Lane Keeping Assistant,Cruise control - speed limiter, Body - color door handles, LED rear lights, Driver - side vanity mirror'),
+(16,'Main','Level 1 Air Conditioning,Model Advantage,Speed regulator,Three modes of Drive'),
+(16,'Standard','Electric AV windows,Imitation leather steering wheel,Heated exterior mirrors,Tire pressure monitoring system, Front seatbelt with pretensioners + force limiters, Lane Keeping Assistant,Cruise control - speed limiter, Body - color door handles, LED rear lights, Driver - side vanity mirror')
+
+
+INSERT INTO tblPaybackTime Values(24),(36),(48),(60)
